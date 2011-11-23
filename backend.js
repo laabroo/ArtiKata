2ba8a635470e5bb1e4511463ba4f1dab74d8f329
unknown <laabroo@.(none)>
2011-11-24 01:50:48 +0700

    view fix, tapi kasih kurang engginenya

--- backend.js
+++ backend.js
@@ -1,2 +1,26 @@
 // ArtiKata -- backend.js
+var http = require('blaast/simple-http');
+var QS = require('querystring');
+
 log.info('Hello from backend bootstrap.');
+
+app.message(function(client, action, data) {
+    console.log(action);
+    console.log(data.text);
+
+    if (action === 'post_data') {
+        var param = "format=json&phrase=" + data.text;
+        var url = "http://kateglo.bahtera.org/api.php?" + param;
+
+        http.get(url, {type : 'binary' }, {
+            ok : function(data) {
+                console.log(data);
+                data = JSON.parse(data);
+                client.msg('post_data', { text : data.data.translations[0].translatedText});
+            },
+            error : function(err) {
+                console.log(err);
+            }
+        });
+    }
+});
--- controller/main.js
+++ controller/main.js
@@ -1,31 +1,50 @@
 var _ = require('common/util');
 
+_.exports[':keypress'] = function(key) {
+    this.textWord.emit('keypress', key);
+    this.valueMean.label('Masuk kata Anda!!');
+};
+
 _.extend(exports, {
-	':load': function() {
-		console.log('View was loaded');
-	},
+    ':load': function() {
+        var view = this;
+        console.log('View was loaded');
 
-	':resized': function(width, height) {
-		console.log('View was resized to ' + width + 'x' + height);
-	},
+        view.get('textWord').on('submit', function() {
+            console.log(view.get('textWord').value());
+            app.message('post_data', {text : view.get('textWord')});
+        });
 
-	':keydown': function(key) {
-		console.log('Key down: '+ key);
-	},
+        app.on('message', function(action, data) {
+            console.log(action);
+            if (action === 'post_data') {
+                console.log(data.text);
+                view.get('valueMean').label(data.text);
+            }
+        });
+    },
 
-	':keyup': function(key) {
-		console.log('Key up: ' + key);
-	},
+    ':resized': function(width, height) {
+        console.log('View was resized to ' + width + 'x' + height);
+    },
 
-	':keypress': function(key) {
-		console.log('Key press: ' + key);
-	},
+    ':keydown': function(key) {
+        console.log('Key down: ' + key);
+    },
 
-	':active': function() {
-		console.log('View is active');
-	},
+    ':keyup': function(key) {
+        console.log('Key up: ' + key);
+    },
 
-	':inactive': function() {
-		console.log('View is inactive');
-	}
+    ':keypress': function(key) {
+        console.log('Key press: ' + key);
+    },
+
+    ':active': function() {
+        console.log('View is active');
+    },
+
+    ':inactive': function() {
+        console.log('View is inactive');
+    }
 });
